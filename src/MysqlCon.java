ae41f37341cc6785779a5363921d87e9f929b1bb
jvcavancini <joaovirgilio123@gmail.com>
2019-03-27 20:35:08 -0300

    Conexão com MySQL adicionada
    Adicionado conexão com mysql, depois vou fazer umas querys basicas pra conectar com o aplicativo.
    
    Bugfix de "endereço"

---
+++ src/MysqlCon.java
@@ -0,0 +1,16 @@
+import java.sql.*;
+class MysqlCon {
+	public static void main(String args[]){  
+		try{  
+		Class.forName("com.mysql.jdbc.Driver");  
+		Connection con=DriverManager.getConnection(  
+		"jdbc:mysql://localhost:3306/youaluga","root","Po2j3kSe96@#ki");
+		Statement stmt=con.createStatement();  
+		ResultSet rs=stmt.executeQuery("select * from usuarios");  
+		while(rs.next())  
+//		System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+//		linha acima pega informações da tabela e printa
+		con.close();  
+		}catch(Exception e){ System.out.println(e);}  
+	}  
+}  
