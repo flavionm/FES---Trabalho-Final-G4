4586e7dd7fb61e85957e9bab0a9435b27e80bcef
Flávio Sousa <fn_maia@outlook.com>
2019-06-05 21:23:21 -0300

    Adicionados mais testes para o ClientController

--- test/Controller/ClientControllerTest.java
+++ test/Controller/ClientControllerTest.java
@@ -12,6 +12,10 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author nuno
@@ -23,12 +27,49 @@ public class ClientControllerTest {
 	
 	@BeforeClass
 	public static void setUpClass() {
+		String sql = "INSERT INTO client (name, email, cpf, phone, cnh, address, city, state)"
+		           + " VALUES ('test', 'test', 'test', 'test', 'test', 'test', 'test', 'test');";
+		try {
+			Connection conn = new ConnectionDB().getConection();
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 	
 	@AfterClass
 	public static void tearDownClass() {
+		String sql = "DELETE FROM client WHERE name = 'test' OR name = 'test2' OR name = 'test3';";
+		try {
+			Connection conn = new ConnectionDB().getConection();
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 
+	/**
+	 * Test of insert method, of class ClientController.
+	 */
+	@Test
+	public void testInsert() {
+		System.out.println("insert");
+		ClientController instance = new ClientController();
+		ArrayList<Client> result = instance.readAll();
+		boolean found = false;
+		for (Client c : result) {
+			if (c.getName().equals("test"))
+				found = true;
+		}
+		assertTrue(found);
+	}
+	
 	/**
 	 * Test of readAll method, of class ClientController.
 	 */
